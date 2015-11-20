470f5a2dbe0a3e5f8e57816f925bef0ccff202a0
takatoh <takatoh.m@gmail.com>
2015-11-21 05:31:01 +0900

    bruschetta/views.py: Update.

--- bruschetta/views.py
+++ bruschetta/views.py
@@ -14,9 +14,17 @@ def book_new():
         book = Book(
             title = request.form['title'],
             volume = request.form['volume'],
+            series = request.form['series'],
+            series_volume = request.form['series_volume'],
             author = request.form['author'],
             translator = request.form['translator'],
-            publisher = request.form['publisher'])
+            publisher = request.form['publisher'],
+            isbn = request.form['isbn'],
+            published_on = request.form['published_on'],
+            original_title = request.form['original_title'],
+            note = request.form['note'],
+            keyword = request.form['keyword'],
+            disk = request.form['disk'])
         db.session.add(book)
         db.session.commit()
         flash('New book was successfully added.')
