df91c8c3c5e3ffc5706ad057b20aeffd56153766
takatoh <takatoh.m@gmail.com>
2015-11-20 23:36:55 +0900

    bruschetta/views.py: Update.

--- bruschetta/views.py
+++ bruschetta/views.py
@@ -8,16 +8,18 @@ def index():
     books = Book.query.order_by(Book.id.desc()).all()
     return render_template('index.html', books=books)
 
-@app.route('/book/add', methods=['POST'])
+@app.route('/book/add', methods=['GET', 'POST'])
 def book_new():
-    book = Book(
-        title = request.form['title'],
-        volume = request.form['volume'],
-        author = request.form['author'],
-        translator = request.form['translator'],
-        publisher = request.form['publisher'])
-    db.session.add(book)
-    db.session.commit()
-    flash('New book was successfully added.')
-    return redirect(url_for('index'))
-
+    if request.method == 'POST':
+        book = Book(
+            title = request.form['title'],
+            volume = request.form['volume'],
+            author = request.form['author'],
+            translator = request.form['translator'],
+            publisher = request.form['publisher'])
+        db.session.add(book)
+        db.session.commit()
+        flash('New book was successfully added.')
+        return redirect(url_for('index'))
+    else:
+        render_template('book_new.html')
