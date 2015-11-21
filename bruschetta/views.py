5abd8c31a2481cba3605e7b9f98ea9d4573c6ac6
takatoh <takatoh.m@gmail.com>
2015-11-21 09:09:00 +0900

    bruschetta/views.py: Add path for Category and Format.

--- bruschetta/views.py
+++ bruschetta/views.py
@@ -1,6 +1,6 @@
 from flask import request, redirect, url_for, render_template, flash
 from bruschetta import app, db
-from bruschetta.models import Book
+from bruschetta.models import Book, Category, Format
 
 
 @app.route('/')
@@ -36,3 +36,27 @@ def book_new():
 def book_detail(book_id):
     book = Book.query.get(book_id)
     return render_template('book_detail.html', book=book)
+
+@app.route('/categories/')
+def category_list():
+    categories = Category.query.all()
+    return render_template('category_list.html', categories=categories)
+
+@app.route('/category/add/', methods=['POST'])
+def category_add():
+    category = Category(name = request.form['name'])
+    db.session.add(category)
+    db.session.commit()
+    return redirect(url_for('category_list'))
+
+@app.route('/formats/')
+def format_list():
+    formats = Format.query.all()
+    return render_template('format_list.html', formats=formats)
+
+@app.route('/format/add/', methods=['POST'])
+def format_add():
+    fmt = Format(name = request.form['name'])
+    db.session.add(fmt)
+    db.session.commit()
+    return redirect(url_for('format_list'))
