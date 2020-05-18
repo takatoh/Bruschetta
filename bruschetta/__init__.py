411ee2c8cff8c5e8c8775d730e4ef49d1dcd764d
takatoh <takatoh.m@gmail.com>
2020-05-19 07:42:21 +0900

    bruschetta/__init__.py: Introduce database migration (Flask-Migrate).

--- bruschetta/__init__.py
+++ bruschetta/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_pyfile('../bruschetta.conf')
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 import bruschetta.views
