fcd539a66d37a41fb4689305b753268bb1874954
takatoh <takatoh.m@gmail.com>
2023-08-19 14:29:15 +0900

    bruschetta/__init__.py: Define variable '__version__'.

--- bruschetta/__init__.py
+++ bruschetta/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+__version__ = 'v0.4-alpha'
+
 
 app = Flask(__name__)
 app.config.from_pyfile('../bruschetta.conf')
