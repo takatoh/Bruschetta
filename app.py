cad94e7c95bc5abf1e0021850515e102ddb73946
takatoh <takatoh.m@gmail.com>
2024-01-08 14:34:22 +0900

    app.py: Add matadata for SQLAlchemy to name constraint.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import MetaData
 
 __version__ = 'v0.4-alpha'
 
@@ -8,7 +9,15 @@ __version__ = 'v0.4-alpha'
 app = Flask(__name__)
 app.config.from_pyfile('./bruschetta.conf')
 
-db = SQLAlchemy(app)
+convention = {
+    'ix': 'ix_%(column_0_label)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(constraint_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'pk': 'pk_%(table_name)s'
+}
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
 
 import models
 
