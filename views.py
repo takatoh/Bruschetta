a29425e9e748fc5ee98ba80deaebb40ff65f0fd0
takatoh <takatoh.m@gmail.com>
2024-01-15 06:38:40 +0900

    views.py(book_edit): Send information of bookshelf.

--- views.py
+++ views.py
@@ -75,6 +75,7 @@ def book_edit(book_id):
     if request.method == 'POST':
         category = Category.query.filter_by(name=request.form['category']).first()
         fmt = Format.query.filter_by(name=request.form['format']).first()
+        bookshelf = BookShelf.query.filter_by(name=request.form['bookshelf']).first()
         book = Book.query.get(book_id)
         book.title          = request.form['title']
         book.volume         = request.form['volume']
@@ -91,6 +92,7 @@ def book_edit(book_id):
         book.note           = request.form['note']
         book.keyword        = request.form['keyword']
         book.disk           = request.form['disk']
+        book.bookshelf_id   = bookshelf.id
         if len(request.form.getlist('disposed')) == 1:
             book.disposed = True
         else:
