4824e58c8365a5417b804a0b2b19b5194a0ede26
takatoh <takatoh.m@gmail.com>
2024-05-11 08:50:30 +0900

    app.py: Re-format by black.

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import MetaData
 
-__version__ = 'v0.4.2'
+__version__ = "v0.4.2"
 
 
 app = Flask(__name__)
-app.config.from_pyfile('./bruschetta.conf')
+app.config.from_pyfile("./bruschetta.conf")
 
 convention = {
-    'ix': 'ix_%(column_0_label)s',
-    'uq': 'uq_%(table_name)s_%(column_0_name)s',
-    'ck': 'ck_%(table_name)s_%(constraint_name)s',
-    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
-    'pk': 'pk_%(table_name)s'
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
 }
 metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(app, metadata=metadata)
