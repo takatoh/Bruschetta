1ce603a2afbb2c6a5709cb1eaddce461bb90d676
takatoh <takatoh.m@gmail.com>
2024-08-13 18:56:45 +0900

    v0.4.3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import MetaData
 
-__version__ = 'v0.4.2'
+__version__ = 'v0.4.3'
 
 
 app = Flask(__name__)
