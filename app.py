e5ce2acd3494857c7dc1a0b11792a467c4970cbc
takatoh <takatoh.m@gmail.com>
2024-10-31 22:13:20 +0900

    {app,models}.py: Connecting database is moved into models module.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sqlalchemy import MetaData
+from models import db
 
 __version__ = "v0.4.2"
 
@@ -9,15 +8,7 @@ __version__ = "v0.4.2"
 app = Flask(__name__)
 app.config.from_pyfile("./bruschetta.conf")
 
-convention = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
-db = SQLAlchemy(app, metadata=metadata)
+db.init_app(app)
 
 migrate = Migrate(app, db)
 
--- models.py
+++ models.py
@@ -1,6 +1,18 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from datetime import datetime
 from timezone import UTC, JST
+
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
 
 
 class Book(db.Model):
