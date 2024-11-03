0ad5e4217b8f5c75d913a23bf8e33fa1f7f598ab
takatoh <takatoh.m@gmail.com>
2024-11-03 13:51:21 +0900

    bruschetta/__init__.py: Set True to instance_relative_config option for application.

--- bruschetta/__init__.py
+++ bruschetta/__init__.py
@@ -5,7 +5,7 @@ __version__ = "v0.4.2"
 
 
 def create_app(config_filename="./bruschetta.conf"):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile(config_filename)
 
     init_app(app)
