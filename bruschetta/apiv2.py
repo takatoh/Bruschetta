5db5709b29d58df211b198d763ba99273db35f3e
takatoh <takatoh.m@gmail.com>
2025-04-29 20:58:05 +0900

    bruschetta/apiv2.py(update_book()): New.

--- bruschetta/apiv2.py
+++ bruschetta/apiv2.py
@@ -62,3 +62,40 @@ def add_book():
     db.session.add(book)
     db.session.commit()
     return jsonify({"status": "OK", "books": [book.to_dictionary()]})
+
+
+@bp.route("/books/<int:book_id>", methods=["PUT"])
+def update_book(book_id):
+    category = Category.query.filter_by(name=request.json["category"]).first()
+    fmt = Format.query.filter_by(name=request.json["format"]).first()
+    if request.json["bookshelf"]:
+        bookshelf_id = (
+            BookShelf.query.filter_by(name=request.json["bookshelf"])
+            .first()
+            .id
+        )
+    else:
+        bookshelf_id = None
+    book = Book.query.get(book_id)
+    book.title = request.json["title"]
+    book.volume = request.json["volume"]
+    book.series = request.json["series"]
+    book.series_volume = request.json["series_volume"]
+    book.author = request.json["author"]
+    book.translator = request.json["translator"]
+    book.publisher = request.json["publisher"]
+    book.category_id = category.id
+    book.format_id = fmt.id
+    book.isbn = request.json["isbn"]
+    book.published_on = request.json["published_on"]
+    book.original_title = request.json["original_title"]
+    book.note = request.json["note"]
+    book.keyword = request.json["keyword"]
+    book.disk = request.json["disk"]
+    book.bookshelf_id = bookshelf_id
+    if len(request.form.getlist("disposed")) == 1:
+        book.disposed = True
+    else:
+        book.disposed = False
+    db.session.commit()
+    return jsonify({"status": "OK", "books": [book.to_dictionary()]})
