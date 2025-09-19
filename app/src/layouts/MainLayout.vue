5676be41552b6305c95e3381345eda210cd5299b
takatoh <takatoh.m@gmail.com>
2025-09-19 20:01:35 +0900

    [app] src/layouts/MainLayout.vue: Change title, and remove Quasar version.

--- app/src/layouts/MainLayout.vue
+++ app/src/layouts/MainLayout.vue
@@ -4,9 +4,7 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title> Bruschetta </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
