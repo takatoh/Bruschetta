15b6633405cccead6a0573d37025729f92e77ca0
takatoh <takatoh.m@gmail.com>
2025-10-18 22:23:39 +0900

    [app] src/pages/IndexPage.vue: Use 'BookListingIgem' component.

--- app/src/pages/IndexPage.vue
+++ app/src/pages/IndexPage.vue
@@ -2,19 +2,13 @@
   <q-page class="flex flex-center">
     <div class="q-pa-md col items-start q-gutter-md">
       <q-list>
-        <template v-for="(book, index) in books" :key="index">
-          <q-card flat bordered class="q-pa-sm" style="width: 600px">
-            <q-card-section class="q-pa-none col-2">
-              {{ book.id }}
-            </q-card-section>
-            <q-card-section class="q-pa-none col-5">
-              {{ book.title }}
-            </q-card-section>
-            <q-card-section class="q-pa-none col-5">
-              {{ book.author }}
-            </q-card-section>
-          </q-card>
-        </template>
+        <book-listing-item
+          v-for="book in books"
+          :key="book.id"
+          :book-id="book.id"
+          :title="book.title"
+          :author="book.author"
+        ></book-listing-item>
       </q-list>
     </div>
   </q-page>
@@ -22,6 +16,7 @@
 
 <script setup>
 import { ref } from 'vue'
+import BookListingItem from 'components/BookListingItem.vue'
 
 const books = ref([
   { id: 1, title: 'The One', author: 'Andy' },
