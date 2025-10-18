7cfdf8c596f67f05b1831e2abffb8d249249e860
takatoh <takatoh.m@gmail.com>
2025-10-18 15:08:50 +0900

    [app] src/pages/CategoryPage.vue: Rewrite.

--- app/src/pages/CategoryPage.vue
+++ app/src/pages/CategoryPage.vue
@@ -1,10 +1,37 @@
 <template>
   <q-page padding>
     <!-- content -->
-    <h2>Categories</h2>
+    <h5 class="text-teal-10">Categories</h5>
+
+    <div class="q-pa-md">
+      <q-table
+        :rows="categories"
+        :columns="categoryColumns"
+        row-key="id"
+        :rows-per-page-options="[0]"
+        bordered
+        dense
+      ></q-table>
+
+      <div>
+        <q-btn label="Add a new category" color="tead" outline></q-btn>
+      </div>
+    </div>
+    "
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+
+const categories = ref([
+  { id: 1, name: 'manga' },
+  { id: 2, name: 'novel' },
+  { id: 3, name: 'sience' },
+])
+
+const categoryColumns = [
+  { name: 'id', label: 'ID', align: 'left', field: (row) => row['id'], format: (val) => `${val}` },
+  { name: 'name', label: 'Name', align: 'left', field: 'name' },
+]
 </script>
