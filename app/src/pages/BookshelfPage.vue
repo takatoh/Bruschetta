bd5b464d56feccbc82f4ff817ea0a82e362a58ea
takatoh <takatoh.m@gmail.com>
2025-10-18 15:29:34 +0900

    [app] src/pages/BookshelfPage.vue: New.

---
+++ app/src/pages/BookshelfPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    <h5 class="text-teal-10">Booksheves</h5>
+
+    <div class="q-pa-md">
+      <q-table
+        :rows="bookshelves"
+        :columns="bookshelfColumns"
+        row-key="id"
+        :rows-per-page-options="[0]"
+        bordered
+        dense
+      ></q-table>
+
+      <div>
+        <q-btn label="Add a new format" color="tead" outline></q-btn>
+      </div>
+    </div>
+    "
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const bookshelves = ref([
+  { id: 1, name: 'Bookshelf 1', description: 'Big.' },
+  { id: 2, name: 'Bookshelf 2', description: 'Middle' },
+  { id: 3, name: 'Bookshelf 3', description: 'Small.' },
+])
+
+const bookshelfColumns = [
+  { name: 'id', label: 'ID', align: 'left', field: (row) => row['id'], format: (val) => `${val}` },
+  { name: 'name', label: 'Name', align: 'left', field: 'name' },
+  { name: 'description', label: 'Description', align: 'left', field: 'description' },
+]
+</script>
