81b1a731def81fc9608181f8b7e546727fb1e0c1
takatoh <takatoh.m@gmail.com>
2025-10-19 10:16:41 +0900

    [app] src/router/routes.js: Add route to 'book/1'.

--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -7,6 +7,7 @@ const routes = [
       { path: 'categories', component: () => import('pages/CategoryPage.vue') },
       { path: 'formats', component: () => import('pages/FormatPage.vue') },
       { path: 'bookshelves', component: () => import('pages/BookshelfPage.vue') },
+      { path: 'book/1', component: () => import('pages/BookDetailPage.vue') },
     ],
   },
 
