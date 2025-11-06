605583fee8f975cd4ea53a266212e048430fa742
takatoh <takatoh.m@gmail.com>
2025-11-06 22:01:08 +0900

    [app] src/pages/IndexPage.vue: Simplify and cleaning.

--- app/src/pages/IndexPage.vue
+++ app/src/pages/IndexPage.vue
@@ -34,16 +34,11 @@
 import { ref } from 'vue'
 import BookListingItem from 'components/BookListingItem.vue'
 
-const books = ref([
-  { id: 1, title: 'The One', author: 'Andy' },
-  { id: 2, title: 'Seconds', author: 'Bill' },
-  { id: 3, title: 'The Third Man', author: 'Charlie' },
-])
+const books = ref([])
 
 const getBooks = async () => {
   const apiRoot = 'http://localhost:5000/api/v2'
   const url = `${apiRoot}/books`
-  console.log(url)
   await fetch(url)
     .then((response) => response.json())
     .then((result) => (books.value = result.books))
