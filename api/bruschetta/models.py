55e8b1196993ebe5d9e54688e9a0a0923003939a
takatoh <takatoh.m@gmail.com>
2025-11-07 22:25:27 +0900

    [api] bruschetta/models.py(Book.as_dict()): Include field 'createdAt'.

--- api/bruschetta/models.py
+++ api/bruschetta/models.py
@@ -97,6 +97,7 @@ class Book(db.Model):
             "coverart": coverart,
             "bookshelf": bookshelf,
             "disposed": self.disposed,
+            "createdAt": self.created_at,
         }
 
     def str_created_at(self):
