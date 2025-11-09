a70dacbb6fd2522c580d0851b5329848a764249e
takatoh <takatoh.m@gmail.com>
2025-11-09 14:01:29 +0900

    [app] Get app name from environment variable 'VUE_APP_NAME'.

--- app/src/boot/ezglobals.js
+++ app/src/boot/ezglobals.js
@@ -4,7 +4,7 @@ import { defineBoot } from '#q-app/wrappers'
 // more info on params: https://v2.quasar.dev/quasar-cli-vite/boot-files
 export default defineBoot(async ({ app }) => {
   // something to do
-  app.config.globalProperties.$APP_NAME = 'Bruschetta-dev'
+  app.config.globalProperties.$APP_NAME = process.env.VUE_APP_NAME
   app.config.globalProperties.$PROJECT_NAME = 'Bruschetta'
   app.config.globalProperties.$PROJECT_VERSION = 'develop'
 })
