35311f9ae9581b53f39ec1811e74a1a025401cb2
takatoh <takatoh.m@gmail.com>
2025-11-20 06:28:03 +0900

    [app] src/pages/CategoryPage.vue: Add a dialog to add a new category.

--- app/src/pages/CategoryPage.vue
+++ app/src/pages/CategoryPage.vue
@@ -14,9 +14,24 @@
       ></q-table>
 
       <div>
-        <q-btn label="Add a new category" color="teal" outline></q-btn>
+        <q-btn label="Add a new category" color="teal" outline @click="openDialog"></q-btn>
       </div>
     </div>
+
+    <q-dialog v-model="dialogOpen" persistent>
+      <q-card style="width: 400px">
+        <q-card-section style="background-color: teal; color: white">
+          <span>Adding a New Cagetory</span>
+        </q-card-section>
+        <q-card-section>
+          <q-input name="name" v-model="categoryNew" label="Name" color="teal"></q-input>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn outline color="teal" label="Cancel" v-close-popup></q-btn>
+          <q-btn outline color="teal" label="Submit" v-close-popup></q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -25,6 +40,8 @@ import { ref } from 'vue'
 import { apiRoot } from 'boot/ezglobals'
 
 const categories = ref([])
+const dialogOpen = ref(false)
+const categoryNew = ref(null)
 
 const categoryColumns = [
   { name: 'id', label: 'ID', align: 'left', field: (row) => row['id'], format: (val) => `${val}` },
@@ -36,6 +53,10 @@ const getCategories = async () => {
   await fetch(url)
     .then((response) => response.json())
     .then((result) => (categories.value = result.categories))
+}
+
+const openDialog = () => {
+  dialogOpen.value = !dialogOpen.value
 }
 
 getCategories()
