44be28dcc446d984f8e12208576da86f64eb7398
takatoh <takatoh.m@gmail.com>
2025-11-20 19:40:38 +0900

    [app] src/components/AddingDialog.vue: New.

---
+++ app/src/components/AddingDialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-dialog persistent>
+    <q-card style="width: 400px">
+      <q-card-section class="bg-teal text-white text-center">
+        <span>{{ props.label }}</span>
+      </q-card-section>
+      <q-card-section>
+        <slot></slot>
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn outline class="text-teal" label="Cancel" v-close-popup></q-btn>
+        <q-btn outline class="text-teal" label="Submit" v-close-popup></q-btn>
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    require: true,
+  },
+})
+</script>
