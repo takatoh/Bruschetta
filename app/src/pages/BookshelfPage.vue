aa73bbe8a982a2b987d7c9c563257cbddad8f58a
takatoh <takatoh.m@gmail.com>
2025-11-22 07:29:11 +0900

    [app] src/pages/BookShelfPage.vue: Implement adding a new bookshelf.

--- app/src/pages/BookshelfPage.vue
+++ app/src/pages/BookshelfPage.vue
@@ -14,17 +14,36 @@
       ></q-table>
 
       <div>
-        <q-btn label="Add a new format" color="teal" outline></q-btn>
+        <q-btn label="Add a new format" color="teal" outline @click="openDialog"></q-btn>
       </div>
     </div>
+
+    <adding-dialog
+      v-model="dialogOpen"
+      label="Adding a New Bookshelf"
+      @submit="addBookshelf"
+      @cancel="cancel"
+    >
+      <q-input name="name" v-model="bookshelfNewName" label="Name" color="teal"></q-input>
+      <q-input
+        name="description"
+        v-model="bookshelfNewDescription"
+        label="Description"
+        color="teal"
+      ></q-input>
+    </adding-dialog>
   </q-page>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { apiRoot } from 'boot/ezglobals'
+import AddingDialog from 'src/components/AddingDialog.vue'
 
 const bookshelves = ref([])
+const dialogOpen = ref(false)
+const bookshelfNewName = ref(null)
+const bookshelfNewDescription = ref(null)
 
 const bookshelfColumns = [
   { name: 'id', label: 'ID', align: 'left', field: (row) => row['id'], format: (val) => `${val}` },
@@ -37,6 +56,46 @@ const getBookshelves = async () => {
   await fetch(url)
     .then((response) => response.json())
     .then((result) => (bookshelves.value = result.bookshelves))
+}
+
+const openDialog = () => {
+  dialogOpen.value = !dialogOpen.value
+}
+
+const addBookshelf = async () => {
+  console.log(`Add a new bookshelf: ${bookshelfNewName.value}`)
+  const url = `${apiRoot}/bookshelves`
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: bookshelfNewName.value,
+      description: bookshelfNewDescription.value,
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error(`Error occured: response status = ${response.status}`)
+      }
+    })
+    .then(() => {
+      clearBookshelfNew()
+      getBookshelves()
+    })
+    .catch((error) => console.log(error))
+}
+
+const cancel = () => {
+  clearBookshelfNew()
+}
+
+const clearBookshelfNew = () => {
+  bookshelfNewName.value = null
+  bookshelfNewDescription.value = null
 }
 
 getBookshelves()
