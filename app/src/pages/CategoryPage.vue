2963f58e0515f2560b28834afb4fb15feee013d2
takatoh <takatoh.m@gmail.com>
2025-11-21 23:25:22 +0900

    [app] src/pages/CategoryPage.vue: Implement adding a new category.

--- app/src/pages/CategoryPage.vue
+++ app/src/pages/CategoryPage.vue
@@ -54,8 +54,34 @@ const openDialog = () => {
   dialogOpen.value = !dialogOpen.value
 }
 
-const addCategory = () => {
+const addCategory = async () => {
   console.log(`Add a ner category: ${categoryNew.value}`)
+  const url = `${apiRoot}/categories`
+  //const formData = new FormData()
+  //formData.append('name', categoryNew.value)
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name: categoryNew.value }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        //console.log('Response OK')
+        return response.json()
+      } else {
+        //console.log('Error: ' + response.status)
+        throw new Error(`Error occured: response status = ${response.status}`)
+      }
+    })
+    .then(() => {
+      //const categ = categories.value.push(result['categories'][0])
+      //console.log(categ.name)
+      categoryNew.value = null
+      getCategories()
+    })
+    .catch((error) => console.log(error))
 }
 
 const cancel = () => {
