00b82fec2b50ebd285d3ffa4c0089a29b289137d
takatoh <takatoh.m@gmail.com>
2025-11-22 06:37:10 +0900

    [app] src/pages/FormatPage.vue: Implement adding a new format.

--- app/src/pages/FormatPage.vue
+++ app/src/pages/FormatPage.vue
@@ -18,7 +18,12 @@
       </div>
     </div>
 
-    <adding-dialog v-model="dialogOpen" label="Adding a New Format">
+    <adding-dialog
+      v-model="dialogOpen"
+      label="Adding a New Format"
+      @submit="addFormat"
+      @cancel="cancel"
+    >
       <q-input name="name" v-model="formatNew" label="Name" color="teal"></q-input>
     </adding-dialog>
   </q-page>
@@ -47,6 +52,34 @@ const getFormats = async () => {
 
 const openDialog = () => {
   dialogOpen.value = !dialogOpen.value
+}
+
+const addFormat = async () => {
+  console.log(`Add a new format: ${formatNew.value}`)
+  const url = `${apiRoot}/formats`
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name: formatNew.value }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error(`Error occured: response status = ${response.status}`)
+      }
+    })
+    .then(() => {
+      formatNew.value = null
+      getFormats()
+    })
+    .catch((error) => console.log(error))
+}
+
+const cancel = () => {
+  formatNew.value = null
 }
 
 getFormats()
