aa4e86d1159908906cbbb710e32774fb4d5ab0ac
takatoh <takatoh.m@gmail.com>
2025-11-22 18:25:52 +0900

    [app] src/pages/IndexPage.vue: Add button to add a new book.

--- app/src/pages/IndexPage.vue
+++ app/src/pages/IndexPage.vue
@@ -15,6 +15,15 @@
         </template>
       </q-input>
     </div>
+    <div>
+      <q-btn
+        outline
+        class="text-teal"
+        label="Add a New Book"
+        v-close-popup
+        @click="openAddingDialog"
+      ></q-btn>
+    </div>
 
     <div class="q-pa-sm col items-start">
       <q-list class="q-gutter-md">
@@ -29,14 +38,18 @@
       </q-list>
     </div>
   </q-page>
+
+  <book-adding-dialog v-model="addingDialogOpen" label="Add a New Book"></book-adding-dialog>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import BookListingItem from 'components/BookListingItem.vue'
+import BookAddingDialog from 'src/components/BookAddingDialog.vue'
 import { apiRoot } from 'boot/ezglobals'
 
 const books = ref([])
+const addingDialogOpen = ref(false)
 
 const getBooks = async () => {
   const params = new URLSearchParams()
@@ -45,6 +58,10 @@ const getBooks = async () => {
   await fetch(url)
     .then((response) => response.json())
     .then((result) => (books.value = result.books))
+}
+
+const openAddingDialog = () => {
+  addingDialogOpen.value = !addingDialogOpen.value
 }
 
 getBooks()
