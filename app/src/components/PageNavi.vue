7ffda595afd972ebac29ebeb6cb037f91c94b412
takatoh <takatoh.m@gmail.com>
2025-11-30 09:18:58 +0900

    [app] src/components/PageNavi.vue: New.

---
+++ app/src/components/PageNavi.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-pagination
+    :model-value="props.currentPage"
+    color="teal"
+    :max="props.maxPage"
+    :max-pages="9"
+    @update:model-value="jumpPage"
+    boundary-numbers
+  ></q-pagination>
+</template>
+<script setup>
+const props = defineProps({
+  currentPage: {
+    type: Number,
+    required: true,
+  },
+  maxPage: {
+    type: Number,
+    required: true,
+  },
+})
+
+const jumpPage = () => {}
+</script>
